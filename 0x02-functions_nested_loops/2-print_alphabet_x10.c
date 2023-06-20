10dab00d3c7f58c95b730b7b5dcdfab4e05d6bfd
asmamaw <asmiesmar50@gmail.com>
2023-06-20 02:12:43 -0700

    prints alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "asme.h"
+
+/**
+ * print_alphabet_x10 - function to print 10 times
+ * return: always void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n')
+	}
+}
