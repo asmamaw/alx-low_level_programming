d62a437ff01e289ded82d991f5761ae1cf87b832
asmamaw <asmiesmar50@gmail.com>
2023-06-26 05:18:39 -0700

    prints arrays of integer elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - a ffunction that prints elements of an array of integers
+ * @a: a pointer to integer
+ * @n:parameter a variable of integer
+ * Return:void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
