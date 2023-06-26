7846d39d18264c4f5e400ca214e1ae39bc00605a
asmamaw <asmiesmar50@gmail.com>
2023-06-26 04:31:37 -0700

    print every other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,17 +4,17 @@
 #include <unistd.h>
 /**
  * puts2 - a function that prints every other string
- * @s: a pointer to string
+ * @str: a pointer to string
  * Return:void
  */
-void puts2(char *s)
+void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
-			putchar(s[i]);
+			putchar(str[i]);
 	}
 	putchar('\0');
 }
