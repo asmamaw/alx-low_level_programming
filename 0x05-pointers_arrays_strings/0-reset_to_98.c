174b4bbdd505aa9b6a0ce018c77aaa928e3fb325
asmamaw <asmiesmar50@gmail.com>
2023-06-26 00:07:02 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * 0-reset_to_98: updates the value it points to 98
+ * 0-reset_to_98.c: updates the value it points to 98
  * return:void
  */
 void reset_to_98(int *n)
 {
-	
 	*n = 98;
 }
