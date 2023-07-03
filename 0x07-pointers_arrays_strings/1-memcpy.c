188b211f99669a5f1bdc2f885d635ef713752022
asmamaw <asmiesmar50@gmail.com>
2023-07-03 02:20:06 -0700

    copies a memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	if ((dest != NULL) && (src != NULL))
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		while (n)
-		{
-		*(dest++) = *(src++);
-		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
