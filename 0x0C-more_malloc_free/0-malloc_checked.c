20eba8e72c1738af5c00bf16a59e27c7651f7818
asmamaw <asmiesmar50@gmail.com>
2023-07-13 01:40:44 -0700

    allocation of memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: size of memory to be allocated
+ * Return: a pointer to address  of allocated nenory space
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		return (98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this more of memory allocation
