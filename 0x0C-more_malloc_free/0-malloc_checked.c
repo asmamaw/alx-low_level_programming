2c499fc1f77acb53c9ddc8f0165edd325bdbbd8c
asmamaw <asmiesmar50@gmail.com>
2023-07-13 02:59:54 -0700

    allocate it

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * malloc_checked - allocate memory using malloc
- * @b: size of memory to be allocated
- * Return: a pointer to address  of allocated nenory space
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
+ * @b: size of the memory block to be allocated
+ *
+ * Return: pointer to the address of the memory block
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *block;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	block = malloc(b);
+	if (block == NULL)
 		exit(98);
-	return (ptr);
+	return (block);
 }
