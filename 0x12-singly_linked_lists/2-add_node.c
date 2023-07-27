2866056bd2fa0acb8ff5a1f3d8c5a14737e3d804
asmamaw <asmiesmar50@gmail.com>
2023-07-27 04:05:35 -0700

    adds elements

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * add_node - adds a new node at the beginning of a list
  *@head: pointer to the head of the list
@@ -8,7 +9,6 @@
  * Return: returns the address to the new element or NULL
  * if failed
 */
-
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
