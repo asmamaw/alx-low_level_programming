8867a71ea369bf58d383885988524b25abd1f06b
asmamaw <asmiesmar50@gmail.com>
2023-08-02 00:17:34 -0700

    more linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - print all the elements of list_t
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *ptr = h;
+	size_t count = 0;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * _putchar - writes the charater c to stdout
+ * @c: the character to print
+ *
+ *Return: on success 1.
+ *on error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
