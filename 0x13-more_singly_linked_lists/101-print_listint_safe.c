2feaed41b24890f07e7d5ff61a815b7d8ab2875a
asmamaw <asmiesmar50@gmail.com>
2023-08-02 06:10:25 -0700

    safe mode

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,17 +7,17 @@
  **/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *ptr = head;
+	size_t pichu = 0;
+	const listint_t *aux_node = head;
 
 	if (!head)
 		exit(98);
 
-	while (ptr)
+	while (aux_node)
 	{
-		printf("[%p] %i\n", (void *)ptr, ptr->n);
-		ptr = ptr->next;
-		nodes++;
+		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
+		aux_node = aux_node->next;
+		pichu++;
 	}
-	return (nodes);
+	return (pichu);
 }
