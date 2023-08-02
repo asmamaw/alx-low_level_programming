31e1d69b79a9260540ca590edaa55bbdd6c4f351
asmamaw <asmiesmar50@gmail.com>
2023-08-02 02:39:34 -0700

    freee listsplease

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * the function sets the head to NULL.
+ * @head: pointer to the list.
+ **/
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+	listint_t *ptr;
+
+	if (head = NULL)
+		return;
+
+	node = *head;
+	ptr = (*head)->next;
+	while (ptr)
+	{
+		free(node);
+		node = ptr;
+		ptr = ptr->next;
+	}
+	free(node);
+	*head = NULL;
+}
